28d4bc05929c3db6ad03b68db4d43b86e802813c
Pavankumartk <119724192+Pavankumartk@users.noreply.github.com>
2026-05-28 13:49:41 +0530

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,44 +10,43 @@ import { Contact } from './entities/contact.entity';
       isGlobal: true,
       envFilePath: '.env',
     }),
+
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE'),
-        entities: [Contact],
-        synchronize: configService.get('NODE_ENV') === 'development',
-      }),
       inject: [ConfigService],
+
+      useFactory: (configService: ConfigService) => {
+        const isProduction =
+          configService.get('NODE_ENV') === 'production';
+
+        return {
+          type: 'postgres',
+
+          ...(isProduction
+            ? {
+                url: configService.get('DATABASE_URL'),
+                ssl: {
+                  rejectUnauthorized: false,
+                },
+              }
+            : {
+                host: configService.get('DB_HOST'),
+                port: parseInt(
+                  configService.get<string>('DB_PORT') || '5432',
+                ),
+                username: configService.get('DB_USERNAME'),
+                password: configService.get('DB_PASSWORD'),
+                database: configService.get('DB_DATABASE'),
+              }),
+
+          entities: [Contact],
+          synchronize: true,
+          autoLoadEntities: true,
+        };
+      },
     }),
+
     ContactsModule,
   ],
 })
 export class AppModule {}
-
-// import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { ConfigModule } from '@nestjs/config';
-
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot({
-//       isGlobal: true,
-//     }),
-
-//     TypeOrmModule.forRoot({
-//       type: 'postgres',
-//       url: process.env.DATABASE_URL,
-//       autoLoadEntities: true,
-//       synchronize: true,
-//       ssl: {
-//         rejectUnauthorized: false,
-//       },
-//     }),
-//   ],
-// })
-// export class AppModule {}
